3bf7166cd6edda8e83deeb392ffc4fc1265fd9d5
Jesuraj77 <isysraju2001@gmail.com>
2020-03-23 17:03:56 +0530

    code Added

---
+++ Level3/Controllers/LoginController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Level3.Controllers
+{
+    public class LoginController : Controller
+    {
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+        public IActionResult Home()
+        {
+            return View();
+        }
+
+        public IActionResult RegistationComplete()
+        {
+            return View();
+        }
+    }
+}
